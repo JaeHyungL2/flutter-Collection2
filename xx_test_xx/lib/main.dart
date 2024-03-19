43a565117890cdbd3c8adcdc6baa5df791c42ee5
JaeHyungL2 <wednestues18@gmail.com>
2024-03-19 17:59:16 +0900

    continue..

--- xx_test_xx/lib/main.dart
+++ xx_test_xx/lib/main.dart
@@ -1,57 +1,12 @@
 import 'package:flutter/material.dart';
 // Include the Google Fonts package to provide more text format options
-// https://pub.dev/packages/google_fonts
+// https://pub.dev/packages/google_fonts 으로 다운받고.
 import 'package:google_fonts/google_fonts.dart';
+
+import 'MyApp.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    const appName = 'Custom Themes';
-
-    return MaterialApp(
-      title: appName,
-      theme: ThemeData(
-        useMaterial3: true,
-
-        // Define the default brightness and colors.
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.purple,
-          // TRY THIS: Change to "Brightness.light"
-          //           and see that all colors change
-          //           to better contrast a light background.
-          brightness: Brightness.dark,
-        ),
-
-        // Define the default `TextTheme`. Use this to specify the default
-        // text styling for headlines, titles, bodies of text, and more.
-        textTheme: TextTheme(
-          displayLarge: const TextStyle(
-            fontSize: 72,
-            fontWeight: FontWeight.bold,
-          ),
-          // TRY THIS: Change one of the GoogleFonts
-          //           to "lato", "poppins", or "lora".
-          //           The title uses "titleLarge"
-          //           and the middle text uses "bodyMedium".
-          titleLarge: GoogleFonts.oswald(
-            fontSize: 30,
-            fontStyle: FontStyle.italic,
-          ),
-          bodyMedium: GoogleFonts.merriweather(),
-          displaySmall: GoogleFonts.pacifico(),
-        ),
-      ),
-      home: const MyHomePage(
-        title: appName,
-      ),
-    );
-  }
 }
 
 class MyHomePage extends StatelessWidget {
